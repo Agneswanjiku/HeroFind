24b0e92971f42d98a5a8aebd398cecf3d35b0509
edithntinyari <emothegi@gmail.com>
2020-11-16 23:50:51 +0300

    added implements method

--- app/src/main/java/com/moringaschool/herofind/MainActivity.java
+++ app/src/main/java/com/moringaschool/herofind/MainActivity.java
@@ -3,8 +3,10 @@ package com.moringaschool.herofind;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
