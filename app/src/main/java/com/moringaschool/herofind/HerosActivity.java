3ea32e98788901bb80b870c6ff8cb1f44f289e5a
edithntinyari <emothegi@gmail.com>
2020-11-17 00:34:02 +0300

    added onClick methods in HerosActivity.java

--- app/src/main/java/com/moringaschool/herofind/HerosActivity.java
+++ app/src/main/java/com/moringaschool/herofind/HerosActivity.java
@@ -2,11 +2,16 @@ package com.moringaschool.herofind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HerosActivity extends AppCompatActivity {
 
@@ -28,5 +33,21 @@ public class HerosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heros);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        MyHerosArrayAdapter adapter = new MyHerosArrayAdapter(this, android.R.layout.simple_list_item_1, parks, animals);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+                String parks = ((TextView)view).getText().toString();
+                Toast.makeText(HerosActivity.this, parks, Toast.LENGTH_LONG).show();
+            }
+        });
+        mLocationTextView.setText("Here are all the parks near: " + location);
     }
+
+
 }
