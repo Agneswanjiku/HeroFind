04d67806d167e48c557d22e020efd4fa3e076c68
edithntinyari <emothegi@gmail.com>
2020-11-16 23:53:44 +0300

    added dependancies in build gradle

--- app/src/main/java/com/moringaschool/herofind/MainActivity.java
+++ app/src/main/java/com/moringaschool/herofind/MainActivity.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
-    
+
+    @BindView(R.id.findHerosButton) Button mFindWildlifesButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
