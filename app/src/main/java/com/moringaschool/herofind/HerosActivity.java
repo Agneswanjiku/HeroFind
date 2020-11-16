1faac4e8bb883e9ea18236b1f07f3b8c8790312d
edithntinyari <emothegi@gmail.com>
2020-11-17 00:24:42 +0300

    commit added BindViews in HerosActivity.java

--- app/src/main/java/com/moringaschool/herofind/HerosActivity.java
+++ app/src/main/java/com/moringaschool/herofind/HerosActivity.java
@@ -3,9 +3,15 @@ package com.moringaschool.herofind;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class HerosActivity extends AppCompatActivity {
 
+    @BindView(R.id.locationTextView) TextView mLocationTextView;
+    @BindView(R.id.listView) ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
