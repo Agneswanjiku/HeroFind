58c2ff9ac2adc1ee554ea0dba43dea820bfe6504
edithntinyari <emothegi@gmail.com>
2020-11-17 00:31:02 +0300

    Added heros String in HerosActivity.java and cultureHerosMythology

--- app/src/main/java/com/moringaschool/herofind/HerosActivity.java
+++ app/src/main/java/com/moringaschool/herofind/HerosActivity.java
@@ -12,6 +12,18 @@ public class HerosActivity extends AppCompatActivity {
 
     @BindView(R.id.locationTextView) TextView mLocationTextView;
     @BindView(R.id.listView) ListView mListView;
+
+
+    private String[] cultureherosmythology = new String[]{"Abenak", "Ainu",
+            "Ausralian Abronigical", "Armenian", "Ashanti", "Aztec",
+            "Banks Islads", "Buddhist", "Carolline Islads", "Lakota",
+            "Maya", "Indian", "Norse",
+            "Navanjo", "Ojibwe"};
+    private String[] heros = new String[]{"  Ant Man", "Aqualman", "Asterix",
+            "The Atom", "The Avengers", "Batgirl", "Batman", "Batwoman", "Black Panary",
+            "Black Panther", "Captain America", "Captain Marvel", "Hellboy", "Doc savage", "Green arrow", "Wonder Woman"};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
