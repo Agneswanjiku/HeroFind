51d2df9a07eadad45c19ed485b33ebb945f44be7
edithntinyari <emothegi@gmail.com>
2020-11-19 16:05:26 +0300

    commit the HeroDetail.java and activity_hero_detail.xml

---
+++ app/src/main/java/com/moringaschool/herofind/HeroDetail.java
@@ -0,0 +1,14 @@
+package com.moringaschool.herofind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HeroDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hero_detail);
+    }
+}
